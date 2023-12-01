6cf0c1976fa78f84fe435d2eccdd22ca14f08881
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2023-12-01 19:23:58 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.42 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+
+        String[] nums = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        String numsString = "";
+        for (int i = 0; i < nums.length; i++) {
+
+            if (numbers.contains(nums[i])) {
+                numbers = numbers.replaceAll(nums[i], String.valueOf(i));
+            }
+        }
+        
+        return Long.parseLong(numbers);
+    }
+}
