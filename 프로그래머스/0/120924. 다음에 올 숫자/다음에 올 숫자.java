de7a684c4ae73df40184bf67fa6b4af7d0b75b22
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2024-01-18 08:27:36 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -5,19 +5,13 @@ class Solution {
         int f = common[1] - common[0];
         int s = common[2] - common[1];
 
-        boolean isGeometricSequence = true;
-
-        if (f == s) {
-            isGeometricSequence = false;
-        }
 
         int len = common.length;
-        if (isGeometricSequence) {
+        if (f == s) {
+            return common[len - 1] + s;
+        } else {
             int divide = common[len - 1] / common[len - 2];
             return common[len - 1] * divide;
-        } else {
-            int subtraction = common[len - 1] - common[len - 2];
-            return common[len - 1] + subtraction;
         }
     }
 }
