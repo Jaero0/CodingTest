3d23cf2ca028bd2f41f5d0b2532ea66bc6e1cf3f
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2024-04-29 15:22:55 +0200

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.41 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -2,29 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
+        int min = Integer.MAX_VALUE;
         List<Integer> answer = new ArrayList<>();
 
-        if (arr.length <= 1) {
-            return new int[]{-1};
-        }
-
-        int min = Integer.MAX_VALUE;
-
         for (int i : arr) {
-
             if (min > i) {
                 min = i;
             }
+            answer.add(i);
         }
 
-        for (int j : arr) {
-
-            if (min == j) {
-                continue;
-            }
-            answer.add(j);
-        }
-
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        answer.remove(Integer.valueOf(min));
+        return answer.isEmpty() ? new int[]{-1} : answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
