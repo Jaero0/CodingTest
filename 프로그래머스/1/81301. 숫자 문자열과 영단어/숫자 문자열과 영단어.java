508c8b442bb65e5231b906355549d88adb83464f
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2024-05-14 12:16:22 +0200

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.36 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,41 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
 
-        StringBuilder sb = new StringBuilder();
-        StringBuilder test = new StringBuilder();
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-        var nums = new HashMap<String, Integer>(){{
-            put("zero", 0);
-            put("one", 1);
-            put("two", 2);
-            put("three", 3);
-            put("four", 4);
-            put("five", 5);
-            put("six", 6);
-            put("seven", 7);
-            put("eight", 8);
-            put("nine", 9);
-        }};
+        for (int i = 0; i < strArr.length; i++) {
 
-        char[] cs = s.toCharArray();
-
-        for (char c : cs) {
-            
-            if ('0' <= c && c <= '9') {
-                sb.append(c);
-                continue;
-            }
-            
-            test.append(c);
-            
-            if (nums.containsKey(test.toString())) {
-                sb.append(nums.get(test.toString()));
-                test.setLength(0);
-            }
+            s = s.replaceAll(strArr[i], String.valueOf(i));
         }
 
-        return Integer.parseInt(sb.toString());
+        return Integer.parseInt(s);
     }
 }
