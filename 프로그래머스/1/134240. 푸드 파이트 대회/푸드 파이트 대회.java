8237d3a4df977b24187bd289204875b39e99ba00
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2024-05-19 10:29:17 +0200

    [level 1] Title: 푸드 파이트 대회, Time: 1.87 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -5,17 +5,12 @@ class Solution {
 
         for (int i = 1; i < food.length; i++) {
 
-            int count = 0;
-            while (count < food[i] / 2) {
-                sb.append(i);
-                count++;
-            }
+            int count = food[i] / 2;
+            sb.append(String.valueOf(i).repeat(count));
         }
-        System.out.println(sb);
 
-        String s1 = sb.toString();
-        String s2 = sb.append(0).reverse().toString();
-        
-        return s1 + s2;
+        String s = sb + "0";
+
+        return s + sb.reverse();
     }
 }
