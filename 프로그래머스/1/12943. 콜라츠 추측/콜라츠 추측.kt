b2e676466596048b016982d98ff8a29582a6b9f6
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2024-06-15 17:06:08 +0200

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
@@ -1,23 +1,13 @@
 class Solution {
 	fun solution(num: Int): Int {
-		var answer = 0
-
-		var col = num.toLong()
-
-		while (answer <= 501) {
-			if (col == 1L) {
-				break
-			}
-			
-			if (col % 2L == 0L) {
-				col /= 2L
-				answer++
-			} else {
-				col = col * 3 + 1
-				answer++
-			}
-		}
-
-		return if (answer > 500) -1 else answer
+		return collatz(num.toLong(), 0)
 	}
+
+	tailrec fun collatz(num:Long, count:Int) : Int =
+		when {
+			count > 500 -> -1
+			num == 1L -> count
+			else -> collatz(if (num % 2 == 0L) num / 2 else num * 3L + 1, count + 1)
+		}
+	
 }
