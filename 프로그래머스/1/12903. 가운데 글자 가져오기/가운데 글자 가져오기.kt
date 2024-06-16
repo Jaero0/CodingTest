c47392b340840fd8654b99b76cda7a16a9555c7b
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2024-06-16 17:51:22 +0200

    [level 1] Title: 가운데 글자 가져오기, Time: 7.77 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
@@ -1,5 +1,4 @@
 class Solution {
 	fun solution(s: String): String = 
-		if (s.length % 2 == 0) s.substring(s.length / 2 - 1, s.length / 2 + 1) 
-		else s.substring(s.length / 2, s.length / 2 + 1)
+		s.substring(s.length / 2 - 1 + (s.length % 2) .. s.length / 2)
 }
