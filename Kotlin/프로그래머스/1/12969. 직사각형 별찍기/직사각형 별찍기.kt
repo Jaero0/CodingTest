2db060a4a63caf32c23fd4a6f15164833b0a4ae0
Jaero0 <124887853+Jaero0@users.noreply.github.com>
2024-07-11 18:19:56 +0200

    [level 1] Title: 직사각형 별찍기, Time: 140.48 ms, Memory: 48.3 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
+++ Kotlin/프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
@@ -2,14 +2,17 @@ import java.io.*
 
 fun main(args: Array<String>) {
 	val br = BufferedReader(InputStreamReader(System.`in`))
+	val bw = BufferedWriter(OutputStreamWriter(System.out))
 	val s = br.readLine()
-	
+
 	val (a, b) = s.split(' ').map { it.toInt() }
 
 	for (i in 1..b) {
 		for (j in 1..a) {
-			print("*")
+			bw.write("*")
 		}
-		println()
+		bw.newLine()
 	}
+	
+	bw.flush()
 }
