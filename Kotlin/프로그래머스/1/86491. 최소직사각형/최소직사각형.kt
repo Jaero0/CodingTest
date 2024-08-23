7b41753f2c5ed89948bfa2ac4b0e1a4f5745821e
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2024-08-23 14:00:59 +0200

    [level 1] Title: 최소직사각형, Time: 13.81 ms, Memory: 68.5 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
+++ Kotlin/프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
@@ -1,41 +1,26 @@
+import kotlin.math.*
+
 class Solution {
     fun solution(sizes: Array<IntArray>): Int {
         
-        var maxX = sizes[0][0]
-        var maxY = sizes[0][1]
-
-        if (maxX < maxY) {
-            val temp = maxX
-            maxX = maxY
-            maxY = temp
-        }
+        var maxX = max(sizes[0][0], sizes[0][1])
+        var maxY = min(sizes[0][0], sizes[0][1])
 
         for (i in sizes.indices) {
 
-            val x = sizes[i][0]
-            val y = sizes[i][1]
+            val x = max(sizes[i][0], sizes[i][1])
+            val y = min(sizes[i][0], sizes[i][1])
 
-            if (y > x) {
-                if (maxX < y) {
-                    maxX = y
+                if (maxX < x) {
+                    maxX = x
                     println("change maxX = $maxX")
                 }
-                if (maxY < x) {
-                    maxY = x
+                if (maxY < y) {
+                    maxY = y
                     println("change maxY = $maxY")
                 }
-                continue
             }
-
-            if (maxX < x) {
-                maxX = x
-                println("change maxX = $maxX")
-            }
-            if (maxY < y) {
-                maxY = y
-                println("change maxY = $maxY")
-            }
-        }
+        
 
         return maxX * maxY
     }
