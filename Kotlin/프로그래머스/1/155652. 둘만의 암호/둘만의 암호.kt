5373af2795d9b665747d1d1cfaa81d5159a816cf
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2024-09-04 16:48:50 +0200

    [level 1] Title: 둘만의 암호, Time: 27.69 ms, Memory: 61.4 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
+++ Kotlin/프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
@@ -2,28 +2,19 @@ class Solution {
     fun solution(s: String, skip: String, index: Int): String {
 
         val sChar = s.toCharArray()
+        val skipSet = skip.toSet()
+
         val sb = StringBuilder()
-        val sSkip = skip.toCharArray().sorted()
 
         for (i in sChar.indices) {
             var char = sChar[i] //y -> a
 
-            for (j in 1 .. index) {
-                char++
-
-                var count = 0
-
-                while (count < sSkip.size) {
+            repeat(index) {
+                do {
+                    char++
+                    println(char)
                     if (char > 'z') char -= 26
-
-                    if (sSkip[count] == char - 26 || sSkip[count] == char) {
-                        char++
-                        count = 0
-                        continue
-                    }
-
-                    count++
-                }
+                } while (char in skipSet)
             }
 
             sb.append(char)
