cb024566879af3f0dae5fe7474f6f615a1346140
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2024-12-05 12:08:38 +0100

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ JavaScript/프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.07 ms
+메모리: 33.5 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 05일 11:46:42
+2024년 12월 05일 12:08:35
 
 ### 문제 설명
 
--- JavaScript/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ JavaScript/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,23 +1,13 @@
-function solution(n) {
-    var answer = 0;
-
-    let list = [];
-
-    while (n > 0) {
-        list.push(n % 10);
-        n /= 10;
-        n = Math.floor(n)
-    }
-
-    list.sort((a, b) => {
-        if (a > b) {
-            return -1
-        } else if (a < b) {
-            return 1;
-        } else {
-            return 0
-        }
-    });
-    
-    return Number(list.join(''));
+function solution(n) {  
+    var answer = 0;  
+  
+    let list = [];  
+  
+    while (n > 0) {  
+        list.push(n % 10);  
+        n = Math.floor(n / 10)  
+    }  
+    list.sort((a, b) => b - a);  
+  
+    return Number(list.join(''));  
 }
