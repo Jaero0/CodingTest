f088c58568b32d59e1fe6dc38551ac12488f1395
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-06-17 08:33:25 +0200

    [level 1] Title: 옹알이 (2), Time: 0.78 ms, Memory: 31.2 MB -BaekjoonHub

--- C#/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cs
+++ C#/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cs
@@ -12,22 +12,19 @@ public class Solution {
             {
                 if(babbling[i].Contains(bab_dou[j]))
                 {
-                    babbling[i] = babbling[i].Replace(bab_dou[j], " ");
+                    babbling[i] = babbling[i].Replace(bab_dou[j], "1");
                     continue;
                 }
                 
                 if(babbling[i].Contains(bab[j]))
                 {
-                    babbling[i] = babbling[i].Replace(bab[j], j.ToString());
+                    babbling[i] = babbling[i].Replace(bab[j], " ");
                 }
             }
             
             string s = babbling[i];
             
-            if(s.Contains(" ")) continue;
-            if(int.TryParse(s, out int aasdsad) == false) continue;
-            
-            answer++;
+            if(s.Trim().Length == 0) answer++;
         }
         
         return answer;
