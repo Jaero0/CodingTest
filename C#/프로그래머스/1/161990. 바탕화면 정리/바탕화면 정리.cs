dfb61c0ea3a8b48c031882fb7c96c753f325bf11
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-06-23 09:46:47 +0200

    [level 1] Title: 바탕화면 정리, Time: 8.00 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ C#/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Solution {
+    public int[] solution(string[] wallpaper) {
+        int[] answer = new int[] {};
+        
+        int minX = wallpaper[0].Length;
+        int minY = wallpaper.Length;
+        int maxX = 0;
+        int maxY = 0;
+        
+        for(int y = 0; y < wallpaper.Length; y++)
+        {
+            int fx = wallpaper[y].IndexOf("#");
+            int lx = wallpaper[y].LastIndexOf("#");
+            
+            if(fx < 0) continue;
+            
+            if(minX > fx) minX = fx;
+            if(maxX < lx) maxX = lx;
+            if(minY > y) minY = y;
+            if(maxY < y) maxY = y;
+            
+            Console.WriteLine(minY + ", " + minX + ", "+(maxY + 1) + ", "+ (maxX + 1));
+        }
+        
+        
+        
+        return new int[] {minY, minX, maxY + 1, maxX + 1};
+    }
+}
