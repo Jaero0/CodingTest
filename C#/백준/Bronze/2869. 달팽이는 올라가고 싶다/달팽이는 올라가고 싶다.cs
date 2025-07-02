6cb6e38a0a6cc3d3768bbe42949a4e5446cd5b2d
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-07-02 07:36:10 +0200

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 48 ms, Memory: 5496 KB -BaekjoonHub

--- C#/백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cs
+++ C#/백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cs
@@ -1,11 +1,12 @@
-
-        string[] inputs = Console.ReadLine().Split(' ');
+string[] inputs = Console.ReadLine().Split(' ');
 
         int climb = int.Parse(inputs[0]);
         int fall = int.Parse(inputs[1]);
         int max = int.Parse(inputs[2]);
 
-        Console.WriteLine((max - fall) % (climb - fall) == 0 ? (max - fall) / (climb - fall) : (max - fall) / (climb - fall) + 1 );
+        var b = (max - fall) % (climb - fall) == 0;
+        var c = (max - fall) / (climb - fall);
+        Console.WriteLine(b ? c : c + 1 );
 
         //n = days
         //-fall * (n - 1) + climb  * n >= max 
