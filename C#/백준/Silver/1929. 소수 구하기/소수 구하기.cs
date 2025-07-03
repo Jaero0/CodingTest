d95199f7d47fc2b10d501a94e85c153c47c198c5
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-07-03 07:40:55 +0200

    [Silver III] Title: 소수 구하기, Time: 3936 ms, Memory: 10080 KB -BaekjoonHub

---
+++ C#/백준/Silver/1929. 소수 구하기/소수 구하기.cs
@@ -0,0 +1,27 @@
+string[] inputs = Console.ReadLine().Split(' ');
+        int f = int.Parse(inputs[0]);
+        int s = int.Parse(inputs[1]);
+
+        bool[] arr = new bool[s + 1];
+        arr[0] = true;
+        arr[1] = true;
+
+        int sqrt = (int)Math.Sqrt(s);
+
+        for (int i = 2; i <= sqrt; i++)
+        {
+            if (arr[i] == false) {
+                for (int j = i * i; j <= s; j+=i)
+                {
+                    arr[j] = true;
+                }
+            }
+        }
+        
+        for (int i = f; i <= s; i++)
+        {
+            if (arr[i] == false)
+            {
+                Console.WriteLine(i);
+            }
+        }
