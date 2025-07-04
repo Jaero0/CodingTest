36cf1d53efc8c814820200055f6d9ee18e4f8dd4
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-07-04 09:00:27 +0200

    [Bronze I] Title: 소인수분해, Time: 52 ms, Memory: 5624 KB -BaekjoonHub

--- C#/백준/Bronze/11653. 소인수분해/소인수분해.cs
+++ C#/백준/Bronze/11653. 소인수분해/소인수분해.cs
@@ -1,34 +1,16 @@
 int input = int.Parse(Console.ReadLine());
-
-        bool[] bools = new bool[input + 1];
-        bools[0] = true;
-        bools[1] = true;
         
+        StreamWriter sw = new (new BufferedStream(Console.OpenStandardOutput()));
         
         for (int i = 2; i * i <= input; i++)
         {
-            for (int j = i * i; j <= input; j+=i)
+            while (input % i == 0)
             {
-                bools[j] = true;
+                input /= i;
+                sw.WriteLine(i);
             }
         }
         
-        StreamWriter sw = new (new BufferedStream(Console.OpenStandardOutput()));
+        if(input > 1) sw.Write(input);
 
-        for (int i = 0; i < bools.Length; i++)
-        {
-            if (bools[i] == false)
-            {
-                while (input % i == 0)
-                {
-                    input /= i;
-                    sw.WriteLine(i);
-                }
-            }
-
-            if (i == input)
-            {
-                break;
-            }
-        }
-        sw.Close();
+        sw.Close(); 
