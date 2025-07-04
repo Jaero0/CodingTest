28143f1dcfe573bd1fcf4bb7dc43b514dbf46098
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-07-04 09:03:53 +0200

    [Bronze I] Title: 소인수분해, Time: 52 ms, Memory: 5544 KB -BaekjoonHub

--- C#/백준/Bronze/11653. 소인수분해/소인수분해.cs
+++ C#/백준/Bronze/11653. 소인수분해/소인수분해.cs
@@ -1,16 +1,24 @@
-int input = int.Parse(Console.ReadLine());
-        
-        StreamWriter sw = new (new BufferedStream(Console.OpenStandardOutput()));
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args) 
+    { 
+        int input = int.Parse(Console.ReadLine());
+
+        StringBuilder sb = new StringBuilder();
         
         for (int i = 2; i * i <= input; i++)
         {
             while (input % i == 0)
             {
                 input /= i;
-                sw.WriteLine(i);
+                sb.Append(i + "\n");
             }
         }
         
-        if(input > 1) sw.Write(input);
+        if(input > 1) sb.Append(input);
 
-        sw.Close(); 
+        Console.WriteLine(sb.ToString());
+    }
+}
