7ce07e8916e21118fd3d1bcaa351d0ead4815e5d
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-07-09 07:56:09 +0200

    [Bronze IV] Title: $A + B - C$, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ C#/백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cs
@@ -0,0 +1,6 @@
+string f = Console.ReadLine();
+        string s = Console.ReadLine();
+        int t = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(int.Parse(f) + int.Parse(s) - t);
+        Console.WriteLine(int.Parse(f + s) - t);
