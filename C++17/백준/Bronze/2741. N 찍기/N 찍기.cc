b332a82ad72c565550262e965dfe3af8e6af02a0
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-08-08 08:06:07 +0200

    [Bronze V] Title: N 찍기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int c; cin >> c;
+
+    for (int i = 1; i <= c; ++i)
+    {
+        cout << i << "\n";
+    }
+    
+    return 0;
+}
