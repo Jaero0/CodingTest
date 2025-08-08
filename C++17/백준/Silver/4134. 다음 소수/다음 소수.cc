45430297fb6c0ba461e0de2cde52ba6c9f2d4871
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-08-08 22:22:22 +0200

    [Silver IV] Title: 다음 소수, Time: 212 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -0,0 +1,51 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int c; cin >> c;
+
+    vector<int> prime;
+
+    for (int i = 0; i < c; ++i)
+    {
+        int64_t input; cin >> input;
+
+        if (input <= 2)
+        {
+            cout << "2" << "\n";
+            continue;
+        }
+        
+        while (true)
+        {
+            bool isPrime = true;
+            
+            for (int64_t i = 2; i * i <= input; ++i)
+            {
+                if (input % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime == false)
+            {
+                input++;
+                continue;
+            }
+
+            cout << input << "\n";
+            break;
+        }
+    }
+    
+    return 0;
+}
+
+
