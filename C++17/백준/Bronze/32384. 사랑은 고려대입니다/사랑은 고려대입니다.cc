20514486f9e4ae3a280f49d94a3f43ef11dc4abd
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-08-09 12:04:50 +0200

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,20 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s = "LoveisKoreaUniversity";
+
+    int c; cin >> c;
+
+    for (int i = 0; i < c; ++i)
+    {
+        cout << s << " ";
+    }
+    
+    
+    return 0;
+}
