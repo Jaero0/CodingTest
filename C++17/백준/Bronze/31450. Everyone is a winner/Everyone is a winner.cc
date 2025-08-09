60c8acf4bd0111d369add9cbeb5198263c2f4dca
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-08-09 12:07:51 +0200

    [Bronze V] Title: Everyone is a winner, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/31450. Everyone is a winner/Everyone is a winner.cc
@@ -0,0 +1,15 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int c, c1; cin >> c >> c1;
+
+    cout << (c % c1 == 0 ? "Yes" : "No");
+    
+    
+    return 0;
+}
