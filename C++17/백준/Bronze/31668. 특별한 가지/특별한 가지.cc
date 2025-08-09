8e460d10304741a7a32ff4b19beb9953609099f4
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-08-09 12:20:05 +0200

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+
+#include "bits/stdc++.h"
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int f,s,t; cin >> f >> s >> t;
+
+    cout << s / f * t;
+    
+    return 0;
+}
