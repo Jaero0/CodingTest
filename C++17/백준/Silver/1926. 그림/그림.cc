42b06d6b5bb2cf0cee18b404f7fdaa241bd29ebe
JaeHo Jeong <124887853+rojae1339@users.noreply.github.com>
2025-08-19 10:20:41 +0200

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3084 KB -BaekjoonHub

--- C++17/백준/Silver/1926. 그림/그림.cc
+++ C++17/백준/Silver/1926. 그림/그림.cc
@@ -1,101 +1,88 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+constexpr int dv[4] = {-1, 1, 0, 0};
+constexpr int dh[4] = {0, 0, -1, 1};
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int v, h; cin >> v >> h;
+    int v, h;
+    cin >> v >> h;
 
-    vector<vector<int>> sq;
+    vector<vector<int>> sq(v, vector<int>(h));
 
-    for (int i = 0; i < v; ++i)
-    {
-        vector<int> ve;
-        for (int i = 0; i < h; ++i)
-        {
-            int t; cin >> t;
-            ve.push_back(t);
+    for (int i = 0; i < v; ++i) {
+        for (int j = 0; j < h; ++j) {
+            cin >> sq[i][j];
         }
-
-        sq.push_back(ve);
     }
 
-    bool visited[500][500] = {false};
-    queue<pair<int,int>> haveToVisit;
+    vector<vector<bool>> visited(v, vector<bool>(h, false));
+    queue<pair<int, int>> haveToVisit;
 
-    vector<int> mins;
+    int componentCount = 0;
+    int maxSize = 0;
 
     for (int i = 0; i < v; ++i)
     {
         for (int j = 0; j < h; ++j)
         {
-            if (visited[i][j] == true)
+            //조건 안맞는거 전부 거르기
+            if (sq[i][j] == 0 || visited[i][j] == true)
             {
                 continue;
             }
 
-            if (sq[i][j] == 1)
+            componentCount++;
+            int componentSize = 0;
+            
+            haveToVisit.emplace(i, j);
+
+            while (!haveToVisit.empty())
             {
-                haveToVisit.emplace(i,j);
+                //v , h
+                auto p = haveToVisit.front();
+                haveToVisit.pop();
 
-                int min = 0;
-
-                while (!haveToVisit.empty())
+                if (visited[p.first][p.second] == true)
                 {
-                    //v , h
-                    auto p = haveToVisit.front();
-                    haveToVisit.pop();
+                    continue;
+                }
 
-                    if (visited[p.first][p.second] == true)
+                visited[p.first][p.second] = true;
+                componentSize++;
+
+                //up,down,left,right check
+                for (int k = 0; k < 4; ++k)
+                {
+                    int movev = p.first + dv[k];
+                    int moveh = p.second + dh[k];
+
+                    if (movev < 0 || movev >= v || moveh < 0 || moveh >= h)
                     {
                         continue;
                     }
 
-                    visited[p.first][p.second] = true;
-
-                    min++;
-
-                    //up,down,left,right check
-                    for (int k = 0; k < 4; ++k)
+                    if (visited[movev][moveh] == true)
                     {
-                        constexpr int dv[4] = {-1,1,0,0};
-                        constexpr int dh[4] = {0,0,-1,1};
+                        continue;
+                    }
 
-                        int movev = p.first + dv[k];
-                        int moveh = p.second + dh[k];
-
-                        if (movev < 0 || movev >= v || moveh < 0 || moveh >= h)
-                        {
-                            continue;
-                        }
-
-                        if (visited[movev][moveh] == true)
-                        {
-                            continue;
-                        }
-
-                        if (sq[movev][moveh] == 1)
-                        {
-                            haveToVisit.emplace(movev, moveh);
-                        }
+                    if (sq[movev][moveh] == 1)
+                    {
+                        haveToVisit.emplace(movev, moveh);
                     }
                 }
-
-                mins.push_back(min);
             }
+
+            maxSize = std::max(componentSize, maxSize);
         }
     }
 
-    int max = 0;
-
-    for (auto i : mins)
-    {
-        max = std::max(max, i);
-    }
-
-    cout << mins.size() << "\n" << max;
-
+    cout << componentCount << "\n" << maxSize;
+    
     return 0;
 }
