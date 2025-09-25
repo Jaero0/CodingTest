fe1c32d11bd9656f0c297cfe3d3b1c6d6ed1a231
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2025-09-25 08:54:29 +0200

    [Silver I] Title: 스티커, Time: 268 ms, Memory: 3980 KB -BaekjoonHub

---
+++ C++17/백준/Silver/9465. 스티커/스티커.cc
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int N; cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        int row; cin >> row;
+
+        vector<vector<int>> sticker(2, vector<int>(row));
+        vector<vector<int>> dp(2, vector<int>(row));
+
+        for (int i = 0; i < row * 2; ++i)
+        {
+            cin >> sticker[i/row][i%row];
+        }
+
+        dp[0][0] = sticker[0][0];
+        dp[1][0] = sticker[1][0];
+        
+        for (int i = 1; i < row; ++i)
+        {
+            dp[0][i] = dp[1][i-1] + sticker[0][i];
+            dp[1][i] = dp[0][i-1] + sticker[1][i];
+
+            if (i - 2 >= 0)
+            {
+                dp[0][i] = max(dp[1][i-2] + sticker[0][i], dp[0][i]);
+                dp[1][i] = max(dp[0][i-2] + sticker[1][i], dp[1][i]);
+            }
+        }
+
+        cout << max(dp[0][row-1], dp[1][row-1]) << "\n";
+    }
+    
+    return 0;
+}
