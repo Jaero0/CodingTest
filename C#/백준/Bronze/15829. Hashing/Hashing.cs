d8e20aadc966e376be095b0d52ffcd83fba28575
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2025-12-11 11:43:56 +0100

    [Bronze II] Title: Hashing, Time: 48 ms, Memory: 5592 KB, Score: 100 point -BaekjoonHub

--- C#/백준/Bronze/15829. Hashing/Hashing.cs
+++ C#/백준/Bronze/15829. Hashing/Hashing.cs
@@ -1,19 +1,26 @@
-StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+namespace Testt;
+
+public class Solution {
+    public static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
         int length = int.Parse(Console.ReadLine());
         string str = Console.ReadLine();
 
-        int r = 1;
+        long r = 1;
         int M = 1234567891;
 
-        int sum = 0;
+        long sum = 0;
         for (int i = 0; i < length; i++)
         {
             int n = str[i] - 'a' + 1;
 
-            sum += ((n % M) * (r % M));
-            r = (r * 31) % M;
+            sum = (sum + n * r % M) % M;
+            r = r * 31 % M;
         }
 
         Console.WriteLine(sum);
+    }
+}
