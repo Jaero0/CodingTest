9a3a2c22b696ef5b94c425d4a6595b8401df9193
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2025-12-11 12:18:20 +0100

    [Silver III] Title: 소수 구하기, Time: 104 ms, Memory: 8228 KB -BaekjoonHub

--- C#/백준/Silver/1929. 소수 구하기/소수 구하기.cs
+++ C#/백준/Silver/1929. 소수 구하기/소수 구하기.cs
@@ -10,42 +10,31 @@ public class Solution {
 
         int[] MN = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
 
-        /*bool[] sosu = new bool[1000001];
+        bool[] sosu = new bool[MN[1] + 1];
         sosu[0] = true;
         sosu[1] = true;
-        for (int i = 2; i * i < 1000000; i++)
+        for (int i = 2; i <= MN[1]; i++)
         {
-            for (int j = i; j < 1000000; j *= 2)
+            if (sosu[i] == true)
             {
-                
+                continue;
             }
-        }*/
+            
+            for (int j = i + i; j <= MN[1]; j += i)
+            {
+                sosu[j] = true;
+            }
+        }
 
         StringBuilder sb = new StringBuilder();
-        
+
         for (int i = MN[0]; i <= MN[1]; i++)
         {
-            if (i == 1)
+            if (sosu[i] == false)
             {
-                continue;
+                sb.Append(i);
+                sb.Append('\n');
             }
-
-            bool isDivided = false;
-            int sqrt = (int)Math.Sqrt(i);
-            for (int j = 2; j <= sqrt; j++)
-            {
-                if (i % j != 0) continue;
-                isDivided = true;
-                break;
-            }
-
-            if (isDivided)
-            {
-                continue;
-            }
-
-            sb.Append(i);
-            sb.Append('\n');
         }
 
         Console.WriteLine(sb.ToString());
