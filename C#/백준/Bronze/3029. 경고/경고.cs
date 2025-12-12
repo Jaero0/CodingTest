5298dbeb83a99c03219b8b745d464e04ceeb71a6
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2025-12-12 12:08:17 +0100

    [Bronze III] Title: 경고, Time: 52 ms, Memory: 5572 KB -BaekjoonHub

---
+++ C#/백준/Bronze/3029. 경고/경고.cs
@@ -0,0 +1,52 @@
+namespace Testt;
+
+public class Solution {
+    public static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int[] first = Array.ConvertAll(sr.ReadLine().Split(':'), int.Parse);
+        int[] second = Array.ConvertAll(sr.ReadLine().Split(':'), int.Parse);
+
+        if (first[0] == second[0] && first[1] == second[1] && first[2] == second[2])
+        {
+            sw.WriteLine("24:00:00");
+            sw.Close();
+            sr.Close();
+            return;
+        }
+
+        int sec, min, hr = 0;
+        if (second[2] < first[2]) //sec
+        {
+            second[1]--;
+            sec = second[2] + 60 - first[2];
+        }
+        else
+        {
+            sec = second[2] - first[2];
+        }
+
+        if (second[1] < first[1])//min
+        {
+            second[0]--;
+            min = second[1] + 60 - first[1];
+        }
+        else
+        {
+            min = second[1] - first[1];
+        }
+
+        hr = second[0] - first[0] < 0 ? second[0] - first[0] + 24 : second[0] - first[0];
+        
+        sw.Write(hr < 10 ? $"0{hr}:" : $"{hr}:");
+        sw.Write(min < 10 ? $"0{min}:" : $"{min}:");
+        sw.Write(sec < 10 ? $"0{sec}" : $"{sec}");
+        
+        sw.Close();
+        sr.Close();
+    }
+    
+    
+}
