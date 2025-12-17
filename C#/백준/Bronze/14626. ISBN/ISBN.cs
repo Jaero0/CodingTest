0e8df370d6468e856b11783a694ce9aa3c05ec69
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2025-12-17 10:24:59 +0100

    [Bronze I] Title: ISBN, Time: 52 ms, Memory: 5576 KB -BaekjoonHub

--- C#/백준/Bronze/14626. ISBN/ISBN.cs
+++ C#/백준/Bronze/14626. ISBN/ISBN.cs
@@ -1,62 +1,46 @@
-StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+namespace Testt;
+
+public class Solution {
+    
+    public static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
-        string s = sr.ReadLine();
+        string isbn = sr.ReadLine();
 
-        int check = s[s.Length - 1] - '0';
-
-        bool isEven = false;
-        
         int sum = 0;
-        for (int i = 0; i < s.Length - 1; i++)
+        int asteriskMul = 0;
+        for (int i = 0; i < 13; i++)
         {
-            if (s[i] == '*')
+            int mul = i % 2 == 0 ? 1 : 3;
+
+            if (isbn[i] == '*')
             {
-                isEven = i % 2 == 0;
+                asteriskMul = mul;
                 continue;
             }
-            
-            int algo = s[i] - '0';
-            
-            if (i % 2 == 0)
-            {
-                sum += algo;
-            }
-            else
-            {
-                sum += algo * 3;
-            }
+
+            sum = ((isbn[i] - '0') * mul + sum) % 10;
         }
-
-        int empty = 0;
-
-        sum += check;
-
-        if (isEven)
+        
+        if (asteriskMul == 1)
         {
-            for (int i = 1; i < 10; i++)
-            {
-                if ((sum + i) % 10 == 0)
-                {
-                    empty = i;
-                    break;
-                }
-            }
+            sw.WriteLine((10 - sum) % 10);
         }
         else
         {
-            for (int i = 1; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
-                if ((sum + i * 3) % 10 == 0)
+                if ((sum + 3 * i) % 10 == 0)
                 {
-                    empty = i;
+                    sw.WriteLine(i);
                     break;
                 }
             }
         }
         
-        sw.WriteLine(empty);
-        
-
-        sr.Close();
         sw.Close();
+        sr.Close();
+    }
+}
