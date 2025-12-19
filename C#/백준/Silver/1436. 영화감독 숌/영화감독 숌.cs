f5e192f5252af94984e31dc4e70dad22df90c493
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2025-12-19 09:39:50 +0100

    [Silver V] Title: 영화감독 숌, Time: 164 ms, Memory: 18484 KB -BaekjoonHub

--- C#/백준/Silver/1436. 영화감독 숌/영화감독 숌.cs
+++ C#/백준/Silver/1436. 영화감독 숌/영화감독 숌.cs
@@ -1,29 +1,37 @@
-StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+using System.Collections;
+
+namespace Testt;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
-        int input = int.Parse(sr.ReadLine());
+        //666 1666 2666 3666 4666 5666 6661 6662 6663 6664 6665 6666 6667 6668 6669 7666 8666 9666
+        //10666 11666 12666 13666 14666 15666 16661 16662 16663 16664 16665 16666 16667 16668 16669 17666 18666 19666
+        //20666
+        //100666 101666 102666 103666
+        //116660
 
-        if (input == 1)
-        {
-            sw.WriteLine(666);
-            sr.Close();
-            sw.Close();
-            return;
-        }
-
-        int force = 666;
+        int n = int.Parse(sr.ReadLine());
+        int ans = 666;
         int count = 1;
 
-        while (count != input)
+        while (count < n)
         {
-            force++;
-            if (force.ToString().Contains("666"))
+            ans++;
+
+            if (ans.ToString().Contains("666"))
             {
                 count++;
             }
         }
         
-        sw.WriteLine(force);
-        
-        sr.Close();
+        sw.WriteLine(ans);
+
         sw.Close();
+        sr.Close();
+    }
+}
