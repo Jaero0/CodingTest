a749b854a1a2182525f7dc7f6baa6b003cffbd52
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2025-12-19 09:59:59 +0100

    [Silver V] Title: 팩토리얼 0의 개수, Time: 52 ms, Memory: 5556 KB -BaekjoonHub

--- C#/백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cs
+++ C#/백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cs
@@ -1,18 +1,46 @@
-StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+using System.Collections;
+
+namespace Testt;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
-        int input = int.Parse(sr.ReadLine());
+        /***
+         * 1
+         * 2 - 2
+         * 3 - 2 * 3
+         * 4 - 2^3 * 3
+         * 5 - 2^3 * 3 * 5
+         * 6 - 2^4 * 3 * 5
+         * 7 - 2^4 * 3 * 5 * 7
+         * 8 - 2^7 * 3 * 5 * 7
+         * 9 - 2^7 * 3^3 * 5 * 7
+         * 10 - 2^8 * 3^3 * 5^2 * 7
+         * 11 - 2^8...5^2
+         * 15 - 2^x * 5^3
+         * ...
+         * 20 - 2^x * 5^4
+         * ...
+         * 25 - 2^x * 5^6
+         */
 
-        int calc = 1;
-        int zeroCount = 0;
+        int N = int.Parse(sr.ReadLine());
 
-        while (input >= 5)
+        int ans = 0;
+
+        while (N > 4)
         {
-            zeroCount += input / 5;
-            input /= 5;
+            N /= 5;
+            ans += N;
         }
         
-        sw.WriteLine(zeroCount);
+        sw.WriteLine(ans);
         
-        sr.Close();
         sw.Close();
+        sr.Close();
+    }
+}
