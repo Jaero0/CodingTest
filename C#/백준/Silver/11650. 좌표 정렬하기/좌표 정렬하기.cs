bbda159a101c4f0d2410de0d008278ee9ba8ad41
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2025-12-23 08:48:26 +0100

    [Silver V] Title: 좌표 정렬하기, Time: 168 ms, Memory: 23272 KB -BaekjoonHub

--- C#/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cs
+++ C#/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cs
@@ -1,27 +1,46 @@
-StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+public class Solution
+{
+    class XY
+    {
+        public int X;
+        public int Y;
+
+        public XY(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
-        int count = int.Parse(sr.ReadLine());
-        var list = new List<int[]>();
-        
-        for (int i = 0; i < count; i++)
-        {
-            int[] input = Array.ConvertAll(sr.ReadLine().Split(' '), s => int.Parse(s));
+        int n = int.Parse(sr.ReadLine());
 
-            list.Add(input);
+        var list = new List<XY>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] arr = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+            list.Add(new XY(arr[0], arr[1]));
         }
         
-        list.Sort((arr, arr1) =>
+        list.Sort((xy1, xy2) =>
         {
-            if (arr[0] == arr1[0])
+            int cmpX = xy1.X - xy2.X;
+
+            if (cmpX == 0)
             {
-                return arr[1] - arr1[1];
+                return xy1.Y - xy2.Y;
             }
 
-            return arr[0] - arr1[0];
+            return cmpX;
         });
         
-        list.ForEach(arr => sw.WriteLine($"{arr[0]} {arr[1]}"));
-
-        sr.Close();
+        list.ForEach(xy => sw.WriteLine(xy.X + " " + xy.Y));
+        
         sw.Close();
+        sr.Close();
+    }
+}
