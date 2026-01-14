c10d6bbdd8616c0ea16c76535a77a05a080f8e18
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-01-14 09:54:40 +0100

    [Silver V] Title: 집합, Time: 844 ms, Memory: 20044 KB -BaekjoonHub

--- C#/백준/Silver/11723. 집합/집합.cs
+++ C#/백준/Silver/11723. 집합/집합.cs
@@ -1,67 +1,67 @@
-StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
-        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+using System.Collections;
+using System.Text;
 
-        int gugu = int.Parse(sr.ReadLine());
-        
-        
-        var dict = new Dictionary<int, bool>()
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int N = int.Parse(sr.ReadLine());
+
+        int[] ar = new int[21];
+
+        for (int i = 0; i < N; i++)
         {
-            {1, false}, {2, false}, {3, false}, {4, false}, {5, false}, {6, false},{7, false}, {8, false},
-            {9, false}, {10, false}, {11, false}, {12, false}, {13, false}, {14, false}, {15, false}, {16, false},
-            {17, false}, {18, false}, {19, false}, {20, false}
-        };
-
-        for (int i = 0; i < gugu; i++)
-        {
-            string[] split = sr.ReadLine().Split(' ');
-            int a = 0;
-
-            if (split.Length == 2)
+            string[] inp = sr.ReadLine().Split(' ');
+            
+            int t = 0;
+            if (inp.Length > 1)
             {
-                a = int.Parse(split[1]);
+                t = int.Parse(inp[1]);
             }
 
-            switch (split[0])
+            if (inp[0] == "add")
             {
-                case "add":
-                    dict[a] = true;
-                    break;
-                case "remove":
-                    dict[a] = false;
-                    break;
-                case "check":
-                    if (dict[a] == true)
-                    {
-                        sw.WriteLine(1);
-                        continue;
-                    }
-                    sw.WriteLine(0);
-                    break;
-                case "toggle":
-                    if (dict[a] == true)
-                    {
-                        dict[a] = false;
-                        continue;
-                    }
-
-                    dict[a] = true;
-                    break;
-                case "all":
-                    for (int j = 1; j <= dict.Count; j++)
-                    {
-                        dict[j] = true;
-                    }
-                    
-                    break;
-                case "empty":
-                    for (int j = 1; j <= dict.Count; j++)
-                    {
-                        dict[j] = false;
-                    }
-                    
-                    break;
+                ar[t]++;
+            }
+            else if (inp[0] == "remove")
+            {
+                if (ar[t] > 0)
+                {
+                    ar[t]--;
+                }
+            }
+            else if (inp[0] == "check")
+            {
+                sw.WriteLine(ar[t] > 0 ? 1 : 0);
+            }
+            else if (inp[0] == "toggle")
+            {
+                if (ar[t] > 0)
+                {
+                    ar[t]--;
+                }
+                else
+                {
+                    ar[t]++;
+                }
+            }
+            else if (inp[0] == "all")
+            {
+                for (int j = 0; j < 21; j++)
+                {
+                    ar[j] = 1;
+                }
+            }
+            else
+            {
+                for (int j = 0; j < 21; j++)
+                {
+                    ar[j] = 0;
+                }
             }
         }
-        
-        sr.Close();
-        sw.Close();
+    }
+}
