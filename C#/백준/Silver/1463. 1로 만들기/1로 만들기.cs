7b4e6a2bbc838d9ba97d46bfb9bd943485bdcccf
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-01-19 10:33:21 +0100

    [Silver III] Title: 1로 만들기, Time: 56 ms, Memory: 9640 KB -BaekjoonHub

--- C#/백준/Silver/1463. 1로 만들기/1로 만들기.cs
+++ C#/백준/Silver/1463. 1로 만들기/1로 만들기.cs
@@ -10,32 +10,24 @@ public class Solution
 
         int N = int.Parse(sr.ReadLine());
 
-        Dictionary<int, int> dic = new Dictionary<int, int>();
-        dic.Add(1, 0);
-        dic.Add(2, 1);
-        dic.Add(3, 1);
+        int[] arr = new int[N + 2];
+        arr[1] = 0;
 
-        for (int i = 4; i <= N; i++)
+        for (int i = 2; i <= N; i++)
         {
-            int count1 = 1;
-            int count2 = int.MaxValue;
-            int count3 = int.MaxValue;
+            arr[i] = arr[i - 1] + 1;
+
             if (i % 2 == 0)
             {
-                count2 = 1;
-                count2 += dic[i / 2];
+                arr[i] = Math.Min(arr[i], arr[i / 2] + 1);
             }
+
             if (i % 3 == 0)
             {
-                count3 = 1;
-                count3 += dic[i / 3];
+                arr[i] = Math.Min(arr[i], arr[i / 3] + 1);
             }
-            
-            count1 += dic[i - 1];
-            
-            dic.Add(i, Math.Min(Math.Min(count1, count2), count3));
         }
         
-        sw.WriteLine(dic[N]);
+        sw.WriteLine(arr[N]);
     }
 }
