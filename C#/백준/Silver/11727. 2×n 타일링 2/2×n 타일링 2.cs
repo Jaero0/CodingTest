9594b6cb573dfab747dda34137bc1cb581c23718
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-01-26 10:07:07 +0100

    [Silver III] Title: 2×n 타일링 2, Time: 52 ms, Memory: 5688 KB -BaekjoonHub

---
+++ C#/백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int N = int.Parse(sr.ReadLine());
+        
+        //(1) 1 3 5 11 21
+        int[] arr = new int[N + 1];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < N + 1; i++)
+        {
+            arr[i] = (arr[i - 1] + arr[i - 2] * 2) % 10007;
+        }
+        
+        sw.WriteLine(arr[N]);
+    }
+}
