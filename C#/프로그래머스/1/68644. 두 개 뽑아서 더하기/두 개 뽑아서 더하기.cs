f9e947d3df95f4d1eb30f373cd667d2b682582e1
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-04-17 16:19:21 +0200

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.88 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] numbers) {
+        var answer = new SortedSet<int>();
+        
+        for(int i = 0; i < numbers.Length - 1; i++)
+        {
+            for(int j = i + 1; j < numbers.Length; j++)
+            {
+                answer.Add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        return answer.ToArray();
+    }
+}
