ccdd392f775c530100b277455028b4a08b8004a6
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-04-17 15:45:36 +0200

    [level 1] Title: 가장 가까운 같은 글자, Time: 0.73 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ C#/프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution {
+    public int[] solution(string s) {
+        int[] answer = new int[s.Length];
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            int count = -1;
+            for(int j = i-1; j >= 0; j--)
+            {
+                if (s[j] == c)
+                {
+                    count = i - j;
+                    break;
+                }
+            }
+            
+            answer[i] = count;
+        }
+        
+        return answer;
+    }
+}
