1b07bead4987fa7a8e99a7973f21f416bd8049b7
JaeHo Jeong <124887853+Jaero0@users.noreply.github.com>
2026-07-07 17:30:17 +0200

    [level 2] Title: 구명보트, Time: 6.31 ms, Memory: 5.13 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) 
+{
+    int answer = 0;
+    
+    sort(people.begin(), people.end());
+    
+    int l = 0;
+    int r = people.size() - 1;
+    
+    while(l <= r)
+    {
+        int sum = people[l] + people[r];
+        
+        if(sum <= limit)
+        {
+            answer++;
+            l++;
+            r--;
+        }
+        else
+        {
+            answer++;
+            r--;
+        }
+    }
+    
+    return answer;
+}
